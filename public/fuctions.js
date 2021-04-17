d9cf93676768ad338b04e8d23daf4b0b5b373423
cosmecr <cosmeneto89@gmail.com>
2021-04-17 17:34:15 -0300

    Create calculadora

---
+++ public/fuctions.js
@@ -0,0 +1,20 @@
+function Soma(){
+  var n1 = prompt("Digite o 1° número");
+  var n2 = prompt("Digite o 2° número");
+  alert( parseInt(n1)+parseInt(n2));
+} 
+function Sub(){
+  var n1 = prompt("Digite o 1° número");
+  var n2 = prompt("Digite o 2° número");
+  alert( parseInt(n1)-parseInt(n2));
+}
+function Mult(){
+  var n1 = prompt("Digite o 1° número");
+  var n2 = prompt("Digite o 2° número");
+  alert( parseInt(n1)*parseInt(n2));
+}
+function Div(){
+  var n1 = prompt("Digite o 1° número");
+  var n2 = prompt("Digite o 2° número");
+  alert( parseInt(n1)/parseInt(n2));
+}
